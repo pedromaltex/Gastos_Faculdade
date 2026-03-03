a241d20d79909e1dc97be957fab50bb57f44e079
Pedro Maltez <pedromaltez16.1@gmail.com>
2026-03-03 16:30:30 +0000

    added buy me a book

--- faculdade_patrimonio.py
+++ faculdade_patrimonio.py
@@ -227,6 +227,26 @@ with tab3:
 # FOOTER
 # -----------------------------
 st.markdown("---")
+import streamlit.components.v1 as components
+
+components.html(
+    """
+    <script type="text/javascript"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+        data-name="bmc-button"
+        data-slug="pedromoraia"
+        data-color="#FFDD00"
+        data-emoji=""
+        data-font="Cookie"
+        data-text="Ajuda-nos a ajudar"
+        data-outline-color="#000000"
+        data-font-color="#000000"
+        data-coffee-color="#ffffff">
+    </script>
+    """,
+    height=80
+)
+
 st.caption(
     "Esta simulação é educativa. Não substitui planeamento financeiro personalizado."
 )
